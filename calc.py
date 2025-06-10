f17138586f571bfb729a7ed15b5b7e7556cba39b
YashM9620 <yashmangatt@gmail.com>
2025-06-10 17:07:13 +0530

    Modified

--- calc.py
+++ calc.py
@@ -1,18 +1,18 @@
-# Simple Calculator
 
-def add(x, y):
-    return x + y
+class SimpleCalculator:
 
-def subtract(x, y):
-    return x - y
+    def add(x, y):
+        return x + y
 
-def multiply(x, y):
-    return x * y
+    def subtract(x, y):
+        return x - y
 
-def divide(x, y):
-    # If the division is by zero
-    if y == 0:
-        return "Division by zero."
-    return x / y
+    def multiply(x, y):
+        return x * y
+
+    def divide(x, y):
+        if y == 0:
+            return "Division by zero."
+        return x / y
 
 
